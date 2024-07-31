318e64015fcca9d03c8ea71f0ed83e37fdf8759b
Shavidu Keshan <shavidu4321@gmail.com>
2024-07-31 11:48:44 +0530

    set the express,mongoose and middlware appjs

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("bye");
+//pasworddb=kgvIiaf0sg7E6vWW
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+//Middleware
+app.use("/", (req, res, next) => {
+    res.send("It is working");
+});
