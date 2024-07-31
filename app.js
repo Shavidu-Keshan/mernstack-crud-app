1bc6f222fb2ad3ae996279abefcbee02f59f5d39
Shavidu Keshan <shavidu4321@gmail.com>
2024-07-31 12:02:43 +0530

    conect the database

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ const app = express();
 app.use("/", (req, res, next) => {
     res.send("It is working");
 });
+
+mongoose.connect("mongodb+srv://admin:kgvIiaf0sg7E6vWW@cluster0.3htd30t.mongodb.net/")
+.then(()=> console.log("connected to Mongodb"))
+.then(() => {
+    app.listen(5000);
+})
+.catch((err)=> console.log((err)))
