6d312fbf8b2723c8a4c0701dce2006bfa4485360
Shavidu Keshan <shavidu4321@gmail.com>
2024-07-31 21:40:28 +0530

    conecct the mongodb

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//pasworddb=kgvIiaf0sg7E6vWW
+//pasworddb=I7FPFrpGfOUpqeyT
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -8,10 +8,16 @@ const app = express();
 app.use("/", (req, res, next) => {
     res.send("It is working");
 });
+app.listen(5000, () => {
+    console.log("Server is running on port 5000");
+});
 
-mongoose.connect("mongodb+srv://admin:kgvIiaf0sg7E6vWW@cluster0.3htd30t.mongodb.net/")
-.then(()=> console.log("connected to Mongodb"))
-.then(() => {
-    app.listen(5000);
+const uri = `mongodb+srv://keshanshavidu:Shavindu2000@shavindu9.1gh7s13.mongodb.net/?retryWrites=true&w=majority&appName=Shavindu9`;
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 })
-.catch((err)=> console.log((err)))
+.then(() => console.log("Connected to MongoDB"))
+
+.catch((err) => console.log(err));
