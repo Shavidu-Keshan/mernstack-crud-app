cd58d497c456ed2832102153228275b10d6fc08a
Shavidu Keshan <shavidu4321@gmail.com>
2024-08-01 08:48:34 +0530

    export route

--- controlers/UserControllers.js
+++ controlers/UserControllers.js
@@ -8,6 +8,14 @@ const getAllUsers = async(req,res,next) => {
     }catch(err){
         console.log(err);
     }
-    //
+    // not found
+    if(!users){
+        return res.status(404).json({ message: "User not found" });
 
-}
+    }
+    //Display all users
+    return res.status(200).json({ users });
+
+};
+
+exports.getAllUsers = getAllUsers;
